0d76799e11297fd6d968d7fcb37a1c8f6c8d595a
John Klein <johnklein514@gmail.com>
2026-01-14 00:31:47 -0600

    sync

--- init.lua
+++ init.lua
@@ -18,6 +18,15 @@ vim.api.nvim_create_autocmd("CursorHold", {
     end,
 })
 
+vim.api.nvim_create_autocmd("TextYankPost", {
+    desc = "Highlight when yanking (copying) text",
+    group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+    callback = function()
+        vim.highlight.on_yank()
+    end
+})
+vim.opt.scrolloff = 10
+
 vim.lsp.inlay_hint.enable(true)
 
 -- copy to system clipboard
@@ -35,8 +44,9 @@ vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
--- Highlight search, disable with :noh
+-- Highlight search, disable with esc
 vim.opt.hlsearch = true
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
 -- Enable terminal colors
 vim.opt.termguicolors = true
--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,4 +1,23 @@
--- Centralized Keymaps Configuration
+-- diagnostic
+vim.keymap.set('n', "<leader>d", vim.diagnostic.open_float, { desc = "show diagnostic" })
+vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Previous diagnostic" })
+vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Next diagnostic" })
+vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Diagnostics to loclist" })
+
+-- lsp
+vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Goto definition" })
+vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { desc = "Goto declaration" })
+vim.keymap.set("n", "gi", vim.lsp.buf.implementation, { desc = "Goto implementation" })
+vim.keymap.set("n", "gr", vim.lsp.buf.references, { desc = "Goto references" })
+vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Hover documentation" })
+vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, { desc = "Signature help" })
+vim.keymap.set("i", "<C-k>", vim.lsp.buf.signature_help, { desc = "Signature help (insert)" })
+
+-- navigation
+vim.keymap.set("n", "<M-h>", "<C-w>h", { desc = "Move to left split" })
+vim.keymap.set("n", "<M-j>", "<C-w>j", { desc = "Move to bottom split" })
+vim.keymap.set("n", "<M-k>", "<C-w>k", { desc = "Move to top split" })
+vim.keymap.set("n", "<M-l>", "<C-w>l", { desc = "Move to right split" })
 
 -- Telescope keymaps
 vim.keymap.set("n", "<leader>ff", function()
@@ -19,8 +38,22 @@ end, { desc = "Help tags" })
 
 vim.keymap.set("n", "<leader>th", ":Telescope themes<CR>", { noremap = true, silent = true, desc = "Theme Switcher" })
 
+
+
 -- NvimTree keymaps
 vim.keymap.set("n", "<leader>e", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" })
+
+-- disable arrow keys
+vim.keymap.set('n', '<left>', '<cmd>echo "ARROW KEYS ARE BANNED"<CR>')
+vim.keymap.set('n', '<right>', '<cmd>echo "ARROW KEYS ARE BANNED"<CR>')
+vim.keymap.set('n', '<up>', '<cmd>echo "ARROW KEYS ARE BANNED"<CR>')
+vim.keymap.set('n', '<down>', '<cmd>echo "ARROW KEYS ARE BANNED"<CR>')
+vim.keymap.set('i', '<left>', '<cmd>echo "ARROW KEYS ARE BANNED"<CR>')
+vim.keymap.set('i', '<right>', '<cmd>echo "ARROW KEYS ARE BANNED"<CR>')
+vim.keymap.set('i', '<up>', '<cmd>echo "ARROW KEYS ARE BANNED"<CR>')
+vim.keymap.set('i', '<down>', '<cmd>echo "ARROW KEYS ARE BANNED"<CR>')
+
+
 
 -- DAP (Debug Adapter Protocol) keymaps
 vim.keymap.set("n", "<F5>", function()
